d6940238bc5bbb206a3a6369d545e8c767a5882b
Arif <arahman.isrt@gmail.com>
2025-03-03 21:01:33 -0600

    update

--- src/components/AppAppBar.jsx
+++ src/components/AppAppBar.jsx
@@ -1,50 +1,49 @@
-import * as React from "react";
-import { alpha, styled } from "@mui/material/styles";
-import { useNavigate } from "react-router-dom";
-import Box from "@mui/material/Box";
-import AppBar from "@mui/material/AppBar";
-import Toolbar from "@mui/material/Toolbar";
-import Button from "@mui/material/Button";
-import IconButton from "@mui/material/IconButton";
-import Container from "@mui/material/Container";
-import Divider from "@mui/material/Divider";
-import MenuItem from "@mui/material/MenuItem";
-import Drawer from "@mui/material/Drawer";
-import MenuIcon from "@mui/icons-material/Menu";
-import CloseRoundedIcon from "@mui/icons-material/CloseRounded";
-import ColorModeIconDropdown from "../theme/ColorModeIconDropdown";
-import Mining4InsightsIcon from "./Mining4InsightsIcon";
-import { useEffect, useState } from "react";
-import { useAuth } from "../contexts/AuthContext";
-
+import * as React from 'react';
+import { alpha, styled } from '@mui/material/styles';
+import { useNavigate } from 'react-router-dom';
+import Box from '@mui/material/Box';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import Container from '@mui/material/Container';
+import Divider from '@mui/material/Divider';
+import MenuItem from '@mui/material/MenuItem';
+import Drawer from '@mui/material/Drawer';
+import MenuIcon from '@mui/icons-material/Menu';
+import CloseRoundedIcon from '@mui/icons-material/CloseRounded';
+import ColorModeIconDropdown from '../theme/ColorModeIconDropdown';
+import Mining4InsightsIcon from './Mining4InsightsIcon';
+import { useEffect, useState } from 'react';
+import { useAuth } from '../contexts/AuthContext';
 
 const StyledToolbar = styled(Toolbar)(({ theme }) => ({
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "space-between",
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'space-between',
   flexShrink: 0,
   borderRadius: `calc(${theme.shape.borderRadius}px + 8px)`,
-  backdropFilter: "blur(24px)",
-  border: "1px solid",
+  backdropFilter: 'blur(24px)',
+  border: '1px solid',
   borderColor: (theme.vars || theme).palette.divider,
   backgroundColor: theme.vars ? `rgba(${theme.vars.palette.background.defaultChannel} / 0.4)` : alpha(theme.palette.background.default, 0.4),
   boxShadow: (theme.vars || theme).shadows[1],
-  padding: "8px 12px",
+  padding: '8px 12px',
 }));
 
 export default function AppAppBar() {
   const navigate = useNavigate();
-  const [loggedIn, setLoggedIn] = useState(!!localStorage.getItem("access_token"));
+  const [loggedIn, setLoggedIn] = useState(!!localStorage.getItem('access_token'));
   const [open, setOpen] = React.useState(false);
   const { isAuthenticated, logout } = useAuth();
 
   const handleLogout = () => {
     logout();
-    navigate("/signin");
+    navigate('/signin');
   };
 
   useEffect(() => {
-    if (localStorage.getItem("access_token")) {
+    if (localStorage.getItem('access_token')) {
       setLoggedIn(true);
     }
   }, []);
@@ -59,18 +58,32 @@ export default function AppAppBar() {
       enableColorOnDark
       sx={{
         boxShadow: 0,
-        bgcolor: "transparent",
-        backgroundImage: "none",
-        mt: "calc(var(--template-frame-height, 0px) + 28px)",
+        bgcolor: 'transparent',
+        backgroundImage: 'none',
+        mt: 'calc(var(--template-frame-height, 0px) + 28px)',
       }}
     >
       <Container maxWidth="lg">
         <StyledToolbar variant="dense" disableGutters>
-          <Box sx={{ flexGrow: 1, display: "flex", alignItems: "center", px: 0 }}>
-            <IconButton onClick={() => navigate("/")}>
+          <Box
+            sx={{
+              flexGrow: 1,
+              display: 'flex',
+              alignItems: 'center',
+              px: 0,
+            }}
+          >
+            <IconButton onClick={() => navigate('/')}>
               <Mining4InsightsIcon />
             </IconButton>
-            <Box sx={{ display: { xs: "none", md: "flex" } }}>
+            <Box
+              sx={{
+                display: {
+                  xs: 'none',
+                  md: 'flex',
+                },
+              }}
+            >
               <Button variant="text" color="info" size="small">
                 Features
               </Button>
@@ -83,32 +96,45 @@ export default function AppAppBar() {
               <Button variant="text" color="info" size="small">
                 Pricing
               </Button>
-              <Button variant="text" color="info" size="small" sx={{ minWidth: 0 }}>
+              <Button
+                variant="text"
+                color="info"
+                size="small"
+                sx={{
+                  minWidth: 0,
+                }}
+              >
                 FAQ
               </Button>
-              <Button variant="text" color="info" size="small" sx={{ minWidth: 0 }}>
+              <Button
+                variant="text"
+                color="info"
+                size="small"
+                sx={{
+                  minWidth: 0,
+                }}
+              >
                 Blog
               </Button>
             </Box>
           </Box>
-          <Box sx={{ display: { xs: "none", md: "flex" }, gap: 1, alignItems: "center" }}>
-            {/* <Button color="primary" variant="text" size="small" onClick={() => {navigate('/signin')}}>Sign in</Button>
-              <Button color="primary" variant="contained" size="small" onClick={() => {navigate('/signup')}}>Sign up</Button> */}
+          <Box
+            sx={{
+              display: {
+                xs: 'none',
+                md: 'flex',
+              },
+              gap: 1,
+              alignItems: 'center',
+            }}
+          >
 
-            {/* {!loggedIn ? (
-                    <>
-                        <Button color="primary" variant="text" size="small" onClick={() => {navigate('/signin')}}>Sign in</Button>
-                        <Button color="primary" variant="contained" size="small" onClick={() => {navigate('/signup')}}>Sign up</Button>
-                    </>
-                ) : ( <Button color="primary" variant="contained" size="small" onClick={handleLogout}>Logout</Button> )
-                } */}
-
-            {!isAuthenticated ? ( // Change loggedIn to isAuthenticated
+            {!isAuthenticated ? (
               <>
-                <Button color="primary" variant="text" size="small" onClick={() => navigate("/signin")}>
+                <Button color="primary" variant="text" size="small" onClick={() => navigate('/signin')}>
                   Sign in
                 </Button>
-                <Button color="primary" variant="contained" size="small" onClick={() => navigate("/signup")}>
+                <Button color="primary" variant="contained" size="small" onClick={() => navigate('/signup')}>
                   Sign up
                 </Button>
               </>
@@ -119,14 +145,41 @@ export default function AppAppBar() {
             )}
             <ColorModeIconDropdown />
           </Box>
-          <Box sx={{ display: { xs: "flex", md: "none" }, gap: 1 }}>
+          <Box
+            sx={{
+              display: {
+                xs: 'flex',
+                md: 'none',
+              },
+              gap: 1,
+            }}
+          >
             <ColorModeIconDropdown size="medium" />
             <IconButton aria-label="Menu button" onClick={toggleDrawer(true)}>
               <MenuIcon />
             </IconButton>
-            <Drawer anchor="top" open={open} onClose={toggleDrawer(false)} PaperProps={{ sx: { top: "var(--template-frame-height, 0px)" } }}>
-              <Box sx={{ p: 2, backgroundColor: "background.default" }}>
-                <Box sx={{ display: "flex", justifyContent: "flex-end" }}>
+            <Drawer
+              anchor="top"
+              open={open}
+              onClose={toggleDrawer(false)}
+              PaperProps={{
+                sx: {
+                  top: 'var(--template-frame-height, 0px)',
+                },
+              }}
+            >
+              <Box
+                sx={{
+                  p: 2,
+                  backgroundColor: 'background.default',
+                }}
+              >
+                <Box
+                  sx={{
+                    display: 'flex',
+                    justifyContent: 'flex-end',
+                  }}
+                >
                   <IconButton onClick={toggleDrawer(false)}>
                     <CloseRoundedIcon />
                   </IconButton>
@@ -137,31 +190,46 @@ export default function AppAppBar() {
                 <MenuItem>Pricing</MenuItem>
                 <MenuItem>FAQ</MenuItem>
                 <MenuItem>Blog</MenuItem>
-                <Divider sx={{ my: 3 }} />
-                <MenuItem>
-                  <Button
-                    color="primary"
-                    variant="contained"
-                    fullWidth
-                    onClick={() => {
-                      navigate("/signup");
-                    }}
-                  >
-                    Sign up1
-                  </Button>
-                </MenuItem>
-                <MenuItem>
-                  <Button
-                    color="primary"
-                    variant="outlined"
-                    fullWidth
-                    onClick={() => {
-                      navigate("/signin");
-                    }}
-                  >
-                    Sign in1
-                  </Button>
-                </MenuItem>
+                <Divider
+                  sx={{
+                    my: 3,
+                  }}
+                />
+
+                {!isAuthenticated ? ( 
+                  <>
+                    <MenuItem>
+                      <Button
+                        color="primary"
+                        variant="outlined"
+                        fullWidth
+                        onClick={() => {
+                          navigate('/signin');
+                        }}
+                      >
+                        Sign in
+                      </Button>
+                    </MenuItem>
+                    <MenuItem>
+                      <Button
+                        color="primary"
+                        variant="contained"
+                        fullWidth
+                        onClick={() => {
+                          navigate('/signup');
+                        }}
+                      >
+                        Sign up
+                      </Button>
+                    </MenuItem>
+                  </>
+                ) : (
+                  <MenuItem>
+                    <Button color="primary" variant="contained" size="small" onClick={handleLogout}>
+                      Logout
+                    </Button>
+                  </MenuItem>
+                )}
               </Box>
             </Drawer>
           </Box>
